88a950dd96bc77a9fb91b9c380de40ce1d459796
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2016-04-06 18:45:34 +0300

    cosmetic cleanup
    Signed-off-by: Hiroshi Doyu <hiroshi.doyu@ericsson.com>

--- server.c
+++ server.c
@@ -11,44 +11,27 @@
 
 int main(int argc, char *argv[])
 {
-	struct sockaddr_un name;
-	int down_flag = 0;
 	int ret;
-	int connection_socket;
-	int data_socket;
-	int result;
-	char buffer[BUFFER_SIZE];
+	int conn;
+	struct sockaddr_un addr = {
+		.sun_family = AF_UNIX,
+		.sun_path = SOCKET_NAME,
+	};
 
 	/*
 	 * In case the program exited inadvertently on the last run,
 	 * remove the socket.
 	 */
-
 	unlink(SOCKET_NAME);
 
 	/* Create local socket. */
-
-	connection_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
-	if (connection_socket == -1) {
+	conn = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+	if (conn == -1) {
 		perror("socket");
 		exit(EXIT_FAILURE);
 	}
 
-	/*
-	 * For portability clear the whole structure, since some
-	 * implementations have additional (nonstandard) fields in
-	 * the structure.
-	 */
-
-	memset(&name, 0, sizeof(struct sockaddr_un));
-
-	/* Bind socket to socket name. */
-
-	name.sun_family = AF_UNIX;
-	strncpy(name.sun_path, SOCKET_NAME, sizeof(name.sun_path) - 1);
-
-	ret = bind(connection_socket, (const struct sockaddr *) &name,
-		   sizeof(struct sockaddr_un));
+	ret = bind(conn, (const struct sockaddr *)&addr, sizeof(addr));
 	if (ret == -1) {
 		perror("bind");
 		exit(EXIT_FAILURE);
@@ -59,82 +42,51 @@ int main(int argc, char *argv[])
 	 * to 20. So while one request is being processed other requests
 	 * can be waiting.
 	 */
-
-	ret = listen(connection_socket, 20);
+	ret = listen(conn, 20);
 	if (ret == -1) {
 		perror("listen");
 		exit(EXIT_FAILURE);
 	}
 
 	/* This is the main loop for handling connections. */
-
 	for (;;) {
+		int data_socket;
+		char buffer[BUFFER_SIZE];
+		int sum = 0;
 
 		/* Wait for incoming connection. */
-
-		data_socket = accept(connection_socket, NULL, NULL);
+		data_socket = accept(conn, NULL, NULL);
 		if (ret == -1) {
 			perror("accept");
 			exit(EXIT_FAILURE);
 		}
 
-		result = 0;
 		for(;;) {
-
 			/* Wait for next data packet. */
-
-			ret = read(data_socket, buffer, BUFFER_SIZE);
+			ret = read(data_socket, buffer, sizeof(buffer));
 			if (ret == -1) {
 				perror("read");
 				exit(EXIT_FAILURE);
 			}
 
-			/* Ensure buffer is 0-terminated. */
-
-			buffer[BUFFER_SIZE - 1] = 0;
-
-			/* Handle commands. */
-
-			if (!strncmp(buffer, "DOWN", BUFFER_SIZE)) {
-				down_flag = 1;
+			if (!strncmp(buffer, "END", 3))
 				break;
-			}
-
-			if (!strncmp(buffer, "END", BUFFER_SIZE)) {
-				break;
-			}
 
 			/* Add received summand. */
-
-			result += atoi(buffer);
+			sum += atoi(buffer);
 		}
 
-		/* Send result. */
-
-		sprintf(buffer, "%d", result);
-		ret = write(data_socket, buffer, BUFFER_SIZE);
-
+		sprintf(buffer, "%d", sum);
+		ret = write(data_socket, buffer, sizeof(buffer));
 		if (ret == -1) {
 			perror("write");
 			exit(EXIT_FAILURE);
 		}
 
-		/* Close socket. */
-
 		close(data_socket);
-
-		/* Quit on DOWN command. */
-
-		if (down_flag) {
-			break;
-		}
 	}
 
-	close(connection_socket);
-
-	/* Unlink the socket. */
-
+	close(conn);
 	unlink(SOCKET_NAME);
-
 	exit(EXIT_SUCCESS);
 }
