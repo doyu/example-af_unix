5108d6b1951f88e7239dc4de7fddba8220bb71eb
Hiroshi Doyu <hiroshi.doyu@ericsson.com>
2016-04-19 16:31:29 +0300

    small update

--- client_netlink.c
+++ client_netlink.c
@@ -7,23 +7,36 @@
 
 int main(int argc, char *argv[])
 {
-	/*           |0123456789abcdef| skip sizeof(nlh)==16 byte */
-	char buf[] = "----------------Hello World!";
+	/*               |0123456789abcdef| skip sizeof(nlmh)==16 byte */
+	char buf[1024] = "----------------Hello World!";
+	struct nlmsghdr *nlh = (struct nlmsghdr *)buf;
 	int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_USERSOCK);
-	struct sockaddr_nl da = {
+	struct sockaddr_nl *sa, da = {
 		.nl_family = AF_NETLINK,
 		.nl_pid = atoi(argv[1]),
 	};
-	struct iovec iov = {
-		.iov_base = (void *)buf,
-		.iov_len = sizeof(buf),
+	struct iovec iov[] = {
+		{
+			.iov_base = (void *)buf,
+			.iov_len = sizeof(buf),
+		},
 	};
 	struct msghdr msg = {
 		.msg_name = (void *)&da,
 		.msg_namelen = sizeof(da),
-		.msg_iov = &iov,
-		.msg_iovlen = 1,
+		.msg_iov = iov,
+		.msg_iovlen = sizeof(iov)/sizeof(iov[0]),
 	};
+
+	nlh->nlmsg_pid = getpid();
+	nlh->nlmsg_len = sizeof(buf);
 	sendmsg(fd, &msg, 0);
+	printf("Sent from my pid=%d\n", nlh->nlmsg_pid);
+
+	recvmsg(fd, &msg, 0);
+	sa = msg.msg_name;
+	printf("Got: %s from pid=%d\n", NLMSG_DATA(buf), sa->nl_pid);
+
+	close(fd);
 	exit(EXIT_SUCCESS);
 }
--- server_netlink.c
+++ server_netlink.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -8,22 +9,35 @@
 int main(int argc, char *argv[])
 {
 	char buf[1024];
+	struct nlmsghdr *nlh = (struct nlmsghdr *)buf;
 	int fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_USERSOCK);
-	struct sockaddr_nl sa = {
+	struct sockaddr_nl *da, sa = {
 		.nl_family = AF_NETLINK,
 		.nl_pid = getpid(),
 	};
-	struct iovec iov =  {
-		.iov_base = (void *)buf,
-		.iov_len = sizeof(buf),
+	struct iovec iov[] =  {
+		{
+			.iov_base = (void *)buf,
+			.iov_len = sizeof(buf),
+		},
 	};
 	struct msghdr msg = {
-		.msg_iov = &iov,
-		.msg_iovlen = 1,
+		.msg_iov = iov,
+		.msg_iovlen = sizeof(iov)/sizeof(iov[0]),
 	};
-	printf("pid=%d\n", sa.nl_pid);
+
+	printf("my pid=%d\n", sa.nl_pid);
 	bind(fd, (struct sockaddr *)&sa, sizeof(sa));
 	recvmsg(fd, &msg, 0);
-	printf("%s\n", NLMSG_DATA(buf));
+	printf("Got: %s from pid=%d\n", NLMSG_DATA(buf), nlh->nlmsg_pid);
+
+	da = &sa;
+	da->nl_pid = nlh->nlmsg_pid;
+	msg.msg_name = (void *)da;
+	msg.msg_namelen = sizeof(*da);
+	strcpy(NLMSG_DATA(buf), "Good-bye!");
+	sendmsg(fd, &msg, 0);
+
+	close(fd);
 	exit(EXIT_SUCCESS);
 }
